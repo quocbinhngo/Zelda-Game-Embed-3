fad265b386ed8dc8f0ba2c9e1352e9a7263a9f28
Binh Ngo <ngobinh0312@gmail.com>
2023-09-26 09:37:48 +0700

    merge sang 26/9

--- src/video/video.c
+++ src/video/video.c
@@ -3,9 +3,10 @@
 #include "video_resources.h"
 #include "../uart.h"
 #include "../mbox.h"
+#include "../time.h"
 
 void video_mode(){
-    framebf_init(SCREEN_WIDTH, SCREEN_HEIGHT, VID_WIDTH, VID_HEIGHT);
+    framebf_init(VID_SCREEN_WIDTH, VID_SCREEN_HEIGHT, VID_WIDTH, VID_HEIGHT);
     display_video(0, 0, VID_WIDTH, VID_HEIGHT, 45, epd_bitmap_allArray);
 }
 
@@ -14,6 +15,6 @@ void display_video(int x, int y, int w, int h, int num_frames, const unsigned lo
     for (int frame = 0; frame < num_frames; frame++)
     {
        drawImage(x, y, w, h, video[frame]);
-       wait_msec(50);
+       wait_msec(100);
     }
 }
